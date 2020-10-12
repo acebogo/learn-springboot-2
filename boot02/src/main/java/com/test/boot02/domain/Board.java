ac5240374d9b5e2d0192c4f8025519b8ab4943dc
acebogo <koprodev@gmail.com>
2020-10-12 23:48:34 +0900

    boot2 기본 셋팅

---
+++ boot02/src/main/java/com/test/boot02/domain/Board.java
@@ -0,0 +1,23 @@
+package com.test.boot02.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@ToString
+public class Board {
+
+    private Long bno;
+    private String title;
+    private String writer;
+    private String content;
+
+    private Timestamp regdate;
+    private Timestamp updatedate;
+
+
+}
